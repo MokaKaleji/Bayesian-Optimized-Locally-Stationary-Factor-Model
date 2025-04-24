671512bd10399811ad0d9ab5dcbfde08ac416b7b
Moka <mohammadkaleji1998@gmail.com>
2025-04-24 18:43:39 +0200

    lsfmestim_big4.m

--- lsfmestim_big4.m
+++ lsfmestim_big4.m
@@ -1,5 +1,5 @@
 %% lsfm_bo_estimation.m
-% Author: Moka Kaleji
+% Author: Moka Kaleji • Contact: mohammadkaleji1998@gmail.com
 % Affiliation: Master Thesis in Econometrics, University of Bologna
 % Description:
 %   Extends LSFM estimation by selecting optimal model hyperparameters
